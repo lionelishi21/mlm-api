473b7f903f882a58b11fe893ac4712e9982cc225
Lionel Francis <lezardvaldth@gmail.com>
2020-11-24 12:58:04 -0500

    updates forgot password

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -9,25 +9,27 @@ use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
-    public function forgot(Request $request) {
-        $credentials = request()->validate(['email' => 'required|email']);
 
-        Password::sendResetLink($credentials);
+    use SendsPasswordResetEmails;
+  
+    // public function forgot(Request $request) {
+        
+    //      $credentials = request()->validate(['email' => 'required|email']);
+    //      Password::sendResetLink($credentials);
+    //     return $this->respondWithMessage('Reset password link sent on your email id.');
+    // }
 
-        return $this->respondWithMessage('Reset password link sent on your email id.');
-    }
 
+    // public function reset(ResetPasswordRequest $request) {
+    //     $reset_password_status = Password::reset($request->validated(), function ($user, $password) {
+    //         $user->password = $password;
+    //         $user->save();
+    //     });
 
-    public function reset(ResetPasswordRequest $request) {
-        $reset_password_status = Password::reset($request->validated(), function ($user, $password) {
-            $user->password = $password;
-            $user->save();
-        });
+    //     if ($reset_password_status == Password::INVALID_TOKEN) {
+    //         return $this->respondBadRequest(ApiCode::INVALID_RESET_PASSWORD_TOKEN);
+    //     }
 
-        if ($reset_password_status == Password::INVALID_TOKEN) {
-            return $this->respondBadRequest(ApiCode::INVALID_RESET_PASSWORD_TOKEN);
-        }
-
-        return $this->respondWithMessage("Password has been successfully changed");
-    }
+    //     return $this->respondWithMessage("Password has been successfully changed");
+    // }
 }
