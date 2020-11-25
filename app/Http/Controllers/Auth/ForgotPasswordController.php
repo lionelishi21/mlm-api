bf46bca28c75111d8bbdb2fe6469b5affdee8484
Lionel Francis <lezardvaldth@gmail.com>
2020-11-25 05:54:08 -0500

    updatea

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,34 +3,31 @@
 namespace App\Http\Controllers\Auth;
 
 use App\ApiCode;
-use App\Http\Controllers\Controller;
 use App\Http\Requests\ResetPasswordRequest;
 use Illuminate\Support\Facades\Password;
 
 
 class ForgotPasswordController extends Controller
 {
+    public function forgot(Request $request) {
+        $credentials = request()->validate(['email' => 'required|email']);
 
-    use SendsPasswordResetEmails;
-  
-    // public function forgot(Request $request) {
-        
-    //      $credentials = request()->validate(['email' => 'required|email']);
-    //      Password::sendResetLink($credentials);
-    //     return $this->respondWithMessage('Reset password link sent on your email id.');
-    // }
+        Password::sendResetLink($credentials);
+
+        return $this->respondWithMessage('Reset password link sent on your email id.');
+    }
 
 
-    // public function reset(ResetPasswordRequest $request) {
-    //     $reset_password_status = Password::reset($request->validated(), function ($user, $password) {
-    //         $user->password = $password;
-    //         $user->save();
-    //     });
+    public function reset(ResetPasswordRequest $request) {
+        $reset_password_status = Password::reset($request->validated(), function ($user, $password) {
+            $user->password = $password;
+            $user->save();
+        });
 
-    //     if ($reset_password_status == Password::INVALID_TOKEN) {
-    //         return $this->respondBadRequest(ApiCode::INVALID_RESET_PASSWORD_TOKEN);
-    //     }
+        if ($reset_password_status == Password::INVALID_TOKEN) {
+            return $this->respondBadRequest(ApiCode::INVALID_RESET_PASSWORD_TOKEN);
+        }
 
-    //     return $this->respondWithMessage("Password has been successfully changed");
-    // }
+        return $this->respondWithMessage("Password has been successfully changed");
+    }
 }
