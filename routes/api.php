5b4f5446aed69ff3991ba4b5e2b4bf8040e469ba
Lionel Francis <lezardvaldth@gmail.com>
2020-12-14 18:13:12 -0500

    updates

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function(){
 
-
-
 	Route::middleware('auth:api')->group(function () { 
 
 		  Route::post('/purchase/booster-packages', 'CustomerController@buyBooster');
